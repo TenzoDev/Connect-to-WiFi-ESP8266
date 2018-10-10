3389ef23aa30a02ba1874e8aecc3f2cce575501c
TenzoDev <34421630+TenzoDev@users.noreply.github.com>
2018-10-10 18:47:12 +0100

    Add files via upload

---
+++ Connect-To-WiFi-ESP8266.ino
@@ -0,0 +1,29 @@
+#include <WiFiEsp.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial Serial1(2,3); // RX, TX
+
+//-------- your wifi -----------
+const char* ssid = "your-wifi-name";
+const char* password = "password-wifi";
+
+void setup() {
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    
+    WiFi.init(&Serial1);
+    Serial.print("Connecting to "); 
+    Serial.print(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    } 
+    Serial.print("WiFi connected, IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+}
+
+
